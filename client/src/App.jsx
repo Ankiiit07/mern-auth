3952396dc03da5e3e8e4d1e53141326e1317aeba
Ankiiit07 <131902683+Ankiiit07@users.noreply.github.com>
2023-08-26 07:32:06 +0530

    redux and google api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+
 export default function App() {
-  return <h1 className='text-red-500 flex-col'>
-    App
-    </h1> 
+  return (
+    <BrowserRouter>
+    {/*header */}
+    <Header/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signUp' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
